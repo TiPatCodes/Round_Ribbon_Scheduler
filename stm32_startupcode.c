da18dce3897bfd42c735bcb84fe1752a35ea712d
Tirth Patel <tirthpatel199561@gmail.com>
2025-12-07 22:10:19 -0500

    1. addition to startup code reset handler

--- stm32_startupcode.c
+++ stm32_startupcode.c
@@ -1,3 +1,22 @@
 #include <stdint.h>
 
-#define SRAM_START  0x2000
+#define SRAM_START     (0x20000000U)
+#define SRAM_SIZE      (1024U * 128U)  // thousand is 1024 we have 128Kb SRAM
+#define SRAM_END       (SRAM_START + SRAM_SIZE)
+#define STACK_START    (SRAM_END)
+
+void Reset_handler(void);
+
+
+uint32_t vectors[] = {
+    STACK_START,
+    (uint32_t)&Reset_handler,
+
+};
+
+
+void Reset_handler(void)
+{
+
+
+}
